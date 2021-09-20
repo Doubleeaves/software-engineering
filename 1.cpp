437b5b63e66fc93a9bcb3147702593b63f89b73c
Kitakaze <2389488343@qq.com>
2021-09-20 21:07:43 +0800

    Create 1.cpp
    测试文件1

---
+++ 1.cpp
@@ -0,0 +1,60 @@
+/* int float 
+   double 
+
+   
+d   da
+ adwsddasw  daw
+   dadddaw
+   dawdaw
+   case\n */
+// return awd 
+// auto a="default"int"
+#include <stdio.h>
+int main(){
+    int i=1;
+    double j=0;
+    long f;
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            
+            break;
+        default:
+            break;
+    }
+    switch(i){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            break;
+    }
+    if(i<0){
+        if(i<-1){}
+        else{};
+    }
+    else if(i>0){
+        if (i>2){}
+        else if (i==2) {}
+        else if (i>1) {}
+        else {}
+    } 
+    else
+    {
+        if(j!=0)
+        {
+            if (1);
+        else if (1);
+        }
+        else{}
+    }
+    if(1)if(1);
+    else if(1);
+    else if(2);
+    else ;
+    return 0;
+}
